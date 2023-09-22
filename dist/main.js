a3ace339c4c3c8288d1ed162c96a23d71ca54e13
aaronburt <email@aaronburt.co.uk>
2023-09-22 23:56:07 +0100

    Commit the main.ts / main.js

--- dist/main.js
+++ dist/main.js
@@ -1,60 +1,47 @@
-// noinspection JSUnusedGlobalSymbols
-
 export default class Postal {
-    private static validatePostalCode(postalCode: string, pattern: RegExp): boolean {
+    static validatePostalCode(postalCode, pattern) {
         return pattern.test(postalCode);
     }
-
-    public static isPostalCode(postalCode: string, pattern: RegExp): boolean {
+    static isPostalCode(postalCode, pattern) {
         return this.validatePostalCode(postalCode, pattern);
     }
-
-    public static isUSPostalCode(postalCode: string): boolean {
+    static isUSPostalCode(postalCode) {
         const usPostalCodePattern = /^\d{5}(-\d{4})?$/;
         return this.isPostalCode(postalCode, usPostalCodePattern);
     }
-
-    public static isCanadaPostalCode(postalCode: string): boolean {
+    static isCanadaPostalCode(postalCode) {
         const canadaPostalCodePattern = /^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$/;
         return this.isPostalCode(postalCode, canadaPostalCodePattern);
     }
-
-    public static isUKPostalCode(postalCode: string): boolean {
+    static isUKPostalCode(postalCode) {
         const ukPostalCodePattern = /^[A-Z]{1,2}[0-9][0-9A-Z]?\s?[0-9][A-Z]{2}$/i;
         return this.isPostalCode(postalCode, ukPostalCodePattern);
     }
-
-    public static isAustraliaPostalCode(postalCode: string): boolean {
+    static isAustraliaPostalCode(postalCode) {
         const australiaPostalCodePattern = /^\d{4}$/;
         return this.isPostalCode(postalCode, australiaPostalCodePattern);
     }
-
-    public static isGermanyPostalCode(postalCode: string): boolean {
+    static isGermanyPostalCode(postalCode) {
         const germanyPostalCodePattern = /^\d{5}$/;
         return this.isPostalCode(postalCode, germanyPostalCodePattern);
     }
-
-    public static isJapanPostalCode(postalCode: string): boolean {
+    static isJapanPostalCode(postalCode) {
         const japanPostalCodePattern = /^\d{3}-\d{4}$/;
         return this.isPostalCode(postalCode, japanPostalCodePattern);
     }
-
-    public static isFrancePostalCode(postalCode: string): boolean {
+    static isFrancePostalCode(postalCode) {
         const francePostalCodePattern = /^(F-)?((2[A|B])|[0-9]{2})[0-9]{3}$/;
         return this.isPostalCode(postalCode, francePostalCodePattern);
     }
-
-    public static isSpainPostalCode(postalCode: string): boolean {
+    static isSpainPostalCode(postalCode) {
         const spainPostalCodePattern = /^(0[1-9]|[1-4][0-9]|5[0-2])\d{3}$/;
         return this.isPostalCode(postalCode, spainPostalCodePattern);
     }
-
-    public static isItalyPostalCode(postalCode: string): boolean {
+    static isItalyPostalCode(postalCode) {
         const italyPostalCodePattern = /^(I-)?\d{5}$/;
         return this.isPostalCode(postalCode, italyPostalCodePattern);
     }
-
-    public static isIndiaPostalCode(postalCode: string): boolean {
+    static isIndiaPostalCode(postalCode) {
         const indiaPostalCodePattern = /^\d{6}$/;
         return this.isPostalCode(postalCode, indiaPostalCodePattern);
     }
